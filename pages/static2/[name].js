5e6632b31f26b402b7503a9e1dfc8edd6af0d751
chenmoonmo <chenmoonmo@gmail.com>
2021-01-19 19:22:36 +0800

    获取随机路由参数

--- pages/static2/[name].js
+++ pages/static2/[name].js
@@ -1,18 +1,32 @@
 import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-const Welecome = () => {
+const Welecome = ({ name2 }) => {
   const router = useRouter()
   const { query = {} } = router || {}
   const { name = '' } = query || {}
   useEffect(() => { }, [])
-  return <div>welecome {name}</div>
+  return (
+    <div>
+      <div>welecome {name}</div><div>welecome {name2}</div>
+    </div>
+  )
+
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { name: '*' } }
+    ],
+    fallback: true
+  }
+}
 
-
-
-
-
+export async function getStaticProps({ params }) {
+  return {
+    props: { name2: params.name }
+  }
+}
 
 export default Welecome
